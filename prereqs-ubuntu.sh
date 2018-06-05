288bd45e4bf2ac2ccf73b1919b84ede3c6ccce14
forgetIt <2271404280@qq.com>
2018-06-05 12:42:40 +0800

    shell complete

--- prereqs-ubuntu.sh
+++ prereqs-ubuntu.sh
@@ -39,79 +39,17 @@ function getOAR() {
 }
 [[ -d ${ORACLIZE} ]] || mkdir ${ORACLIZE}
 cd ${ORACLIZE}
-[[ "`ls -A`" == "" ]] || truffle init
-[[ "`ls -A installed_contracts`" == "" ]] || truffle install oraclize-api
+[[ "`ls -A`" != "" ]] || truffle init
+[[ "`ls -A installed_contracts`" == "oraclize-api" ]] || truffle install oraclize-api
 cat "${DIR}/config/2_initial_migration.js" | tee "${ORACLIZE}/migrations/2_initial_migration.js"
 cat "${DIR}/config/truffle.js" | tee "${ORACLIZE}/truffle.js"
 (
     getOAR
     echo "using ${OAR} and deploying OraclizeTest.sol"
     # https://github.com/WWWillems/medium-02-truffle-oraclize-api
-    tee "${ORACLIZE}/contracts/OraclizeTest.sol" <<EOF
-pragma solidity ^0.4.21;
-import "installed_contracts/oraclize-api/contracts/usingOraclize.sol";
-
-contract OraclizeTest is usingOraclize {
-
-    address owner;
-    string public ETHUSD;
-
-    event LogInfo(string description);
-    event LogPriceUpdate(string price);
-    event LogUpdate(address indexed _owner, uint indexed _balance);
-
-    // Constructor
-    function OraclizeTest()
-    payable
-    public {
-        owner = msg.sender;
-
-        emit LogUpdate(owner, address(this).balance);
-
-        // Replace the next line with your version:
-        OAR = OraclizeAddrResolverI(${OAR=0x98d52C3C3959B35496477510920e2C99E6e9cAC0});
-
-        oraclize_setProof(proofType_TLSNotary | proofStorage_IPFS);
-        update();
-    }
-
-    // Fallback function
-    function()
-    public{
-        revert();
-    }
-
-    function __callback(bytes32 myid, string result, bytes proof)
-    public {
-        require(msg.sender == oraclize_cbAddress());
-
-        ETHUSD = result;
-        emit LogPriceUpdate(ETHUSD);
-        update();
-    }
-
-    function getBalance()
-    public
-    returns (uint _balance) {
-        return address(this).balance;
-    }
-
-    function update()
-    payable
-    public {
-        // Check if we have enough remaining funds
-        if (oraclize_getPrice("URL") > address(this).balance) {
-            emit LogInfo("Oraclize query was NOT sent, please add some ETH to cover for the query fee");
-        } else {
-            emit LogInfo("Oraclize query was sent, standing by for the answer..");
-
-            // Using XPath to to fetch the right element in the JSON response
-            oraclize_query("URL", "json(https://api.coinbase.com/v2/prices/ETH-USD/spot).data.amount");
-        }
-    }
-
-}
-EOF
+    # tee "${ORACLIZE}/contracts/OraclizeTest.sol" <<EOF...${OAR=...}...EOF
+    sed -i s/'${OAR}'/"${OAR}"/ OraclizeTest.sol
+    cat "${DIR}/configOraclizeTest.sol" | tee "${ORACLIZE}/contracts/OraclizeTest.sol"
     [[ -d ./build ]] || rm -rf ./build
     truffle compile
     truffle migrate --develop --reset
