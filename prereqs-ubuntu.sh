187da12c06fcde175839e51c84a2211a2f738c5d
forgetIt <2271404280@qq.com>
2018-06-06 10:21:34 +0800

    2018-06-06 10:21:34

--- prereqs-ubuntu.sh
+++ prereqs-ubuntu.sh
@@ -7,7 +7,7 @@ export NVM_DIR="${HOME}/.nvm"
 if ! nvm --version; then
     if [[ ! -d ${NVM_DIR} || ! -s "${NVM_DIR}/nvm.sh" || ! -s "${NVM_DIR}/bash_completion" ]]; then
         echo "# Installing nvm dependencies"
-        sudo apt-get -y install build-essential libssl-dev
+        sudo apt -y install build-essential libssl-dev
         echo "# Executing nvm installation script"
         curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
     fi
@@ -16,17 +16,17 @@ fi
 [ -s "${NVM_DIR}/nvm.sh" ] && . "${NVM_DIR}/nvm.sh"
 [ -s "${NVM_DIR}/bash_completion" ] && . "${NVM_DIR}/bash_completion"
 
-sudo apt-get -y install language-pack-zh-hans
+sudo apt -y install language-pack-zh-hans
 node -v || nvm install --lts
 nvm ls-remote --lts | grep $(node -v) || nvm use --lts && nvm alias default 'lts/*'
-node-gyp -v || sudo apt-get install -y node-gyp
+node-gyp -v || sudo apt install -y node-gyp
 truffle version || npm install -g truffle --registry=https://registry.npm.taobao.org
 ethereum-bridge --version || npm install -g ethereum-bridge --registry=https://registry.npm.taobao.org
 # ***************************************************************
 if ! docker -v; then
-    sudo apt-get -y install docker.io
-    sudo usermod -aG docker $(whoami)
+    sudo apt -y install docker.io
 fi
+sudo usermod -aG docker $(whoami)
 [[ -d /etc/docker ]] || sudo mkdir -p /etc/docker
 sudo tee /etc/docker/daemon.json <<-'EOF'
 {
@@ -40,7 +40,7 @@ set +e
 COUNT="$(python -V 2>&1 | grep -c 2.)"
 if [ ${COUNT} -ne 1 ]
 then
-   sudo apt-get install -y python-minimal
+   sudo apt install -y python-minimal
 fi
 set -e
 docker-compose --version || sudo pip install docker-compose==1.13.0
@@ -50,9 +50,10 @@ rm -rf qdata/
 ./raft-init.sh
 ./raft-start.sh
 # ***************************************************************
-git clone https://github.com/blk-io/blk-explorer-free.git
+[[ -d blk-explorer-free || "`ls -A`" != "" ]] || git clone https://github.com/blk-io/blk-explorer-free.git
 NODE_ENDPOINT=http://localhost:22000 docker-compose -f "${DIR}/blk-explorer-free/linux-docker-compose.yaml" up -d
 # ***************************************************************
+cd ${DIR}
 npm run setup
 npm run server
 # ***************************************************************
