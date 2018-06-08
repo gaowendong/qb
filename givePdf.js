a385c23220e3e4fb5ca31bade29749c134896ff6
forgetIt <2271404280@qq.com>
2018-06-08 15:44:36 +0800

    2018-06-08 15:44:36

--- givePdf.js
+++ givePdf.js
@@ -7,6 +7,7 @@ const Send = require('koa-send');
 curl -o givePdf.js https://raw.githubusercontent.com/fogetIt/qb/master/givePdf.js
 sudo apt -y install build-essential libssl-dev
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
 [ -s "${NVM_DIR}/nvm.sh" ] && \. "${NVM_DIR}/nvm.sh"
 [ -s "${NVM_DIR}/bash_completion" ] && \. "${NVM_DIR}/bash_completion"
 nvm install --lts
