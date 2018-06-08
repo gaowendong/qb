5a1bbefe43ebbc682d5a8df04cf3682dfd85690b
forgetIt <2271404280@qq.com>
2018-06-08 09:48:37 +0800

    2018-06-08 09:48:37

--- 7nodes.sh
+++ 7nodes.sh
@@ -14,12 +14,12 @@ cd /home/vagrant
 curl -sLf https://raw.githubusercontent.com/jpmorganchase/quorum-examples/master/vagrant/bootstrap.sh | bash
 # error info
 # cp: cannot stat '/vagrant/examples': No such file or directory
-passwd vagrant
-su vagrant
 mkdir /home/vagrant/examples
 git clone https://github.com/jpmorganchase/quorum-examples.git /home/vagrant/examples
 cp -r /home/vagrant/examples/examples /home/vagrant/quorum-examples
 chown -R vagrant:vagrant /home/vagrant/quorum /home/vagrant/quorum-examples
+passwd vagrant
+su vagrant
 curl -sLf https://raw.githubusercontent.com/fogetIt/qb/master/7nodes.sh | bash
 COMMIT
 # Deploying in virtualbox
