890d54d765d3a931ab15438356e44bfd93798081
forgetIt <2271404280@qq.com>
2018-06-08 09:42:14 +0800

    2018-06-08 09:42:14

--- 7nodes.sh
+++ 7nodes.sh
@@ -18,12 +18,12 @@ curl -sLf https://raw.githubusercontent.com/jpmorganchase/quorum-examples/master
 # cp: cannot stat '/vagrant/examples': No such file or directory
 passwd vagrant
 su vagrant
-curl -sLf https://raw.githubusercontent.com/fogetIt/qb/master/7nodes.sh | bash
+curl -sLf https://raw.githubusercontent.com/fogetIt/qb/master/7nodes.sh | sudo bash
 COMMIT
     [[ -d /home/vagrant/examples ]] || mkdir /home/vagrant/examples
     git clone https://github.com/jpmorganchase/quorum-examples.git /home/vagrant/examples
     cp -r /home/vagrant/examples/examples /home/vagrant/quorum-examples
-    chown -R vagrant:vagrant /home/vagrant/quorum /home/vagrant/quorum-examples
+    sudo chown -R vagrant:vagrant /home/vagrant/quorum /home/vagrant/quorum-examples
 fi
 : <<'COMMIT'
 git clone https://github.com/jpmorganchase/quorum-examples
