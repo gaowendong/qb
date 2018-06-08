b241a9c5ddf61c0f918c033acc2eb9b1019687f8
forgetIt <2271404280@qq.com>
2018-06-08 15:31:04 +0800

    2018-06-08 15:31:04

---
+++ givePdf.js
@@ -0,0 +1,30 @@
+'use strict';
+const fs = require('fs');
+const Koa = require('koa');
+const Send = require('koa-send');
+
+/*
+sudo apt -y install build-essential libssl-dev
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+[ -s "${NVM_DIR}/nvm.sh" ] && \. "${NVM_DIR}/nvm.sh"
+[ -s "${NVM_DIR}/bash_completion" ] && \. "${NVM_DIR}/bash_completion"
+nvm install --lts
+nvm use --lts && nvm alias default 'lts/*'
+sudo apt update
+sudo apt install -y node-gyp
+npm install koa koa-send
+npm install pm2 -g
+pm2 start givePdf.js
+*/
+
+const app = new Koa();
+const PORT = 8765;
+
+app.use(Json());
+app.use(async (ctx, next) => {
+    const fileName = 'response.json';
+    ctx.attachment(fileName);
+    await Send(ctx, fileName, { root: __dirname });
+});
+app.listen(PORT);
+console.log("Server started and listen on port " + PORT);
